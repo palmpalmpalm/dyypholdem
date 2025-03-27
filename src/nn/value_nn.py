88a5d7431058c8e18f59e68aeb4028c035c55615
Supawich Sitdhipol <supawichs@gmail.com>
2025-03-27 07:18:43 +0700

    fix: revert to dyypholdem

--- src/nn/value_nn.py
+++ src/nn/value_nn.py
@@ -2,16 +2,15 @@
 import torch
 
 import settings.arguments as arguments
-import os
+
 import nn.modules.module
-from nn.my_model import BaselineNN
+
 
 class ValueNn(object):
 
     model_info: dict
     model_state: dict
-    is_my_model: bool
-    model: object
+    model: nn.modules.module
 
     def __init__(self):
         self.model_info = {}
@@ -30,13 +29,7 @@ class ValueNn(object):
     # -- @param output An NxO tensor in which to store N sets of neural net outputs.
     # -- See @{net_builder} for details of each output.
     def get_value(self, inputs, output):
-        if self.is_my_model:
-            # print(inputs.unsqueeze(1).shape)
-            with torch.no_grad():
-                # print(self.model(inputs.unsqueeze(1)).squeeze(1).shape)
-                output.copy_(self.model(inputs.unsqueeze(1)).squeeze(1))            
-        else:
-            output.copy_(self.model.forward(inputs))
+        output.copy_(self.model.forward(inputs))
 
     def load_for_street(self, street, aux=False, training=False):
         if training:
@@ -48,36 +41,11 @@ class ValueNn(object):
             assert street == 1
             net_file = net_file + "preflop-aux/"
         else:
-            if (street == 3):
-                return self.load_river_model()    
             net_file += arguments.street_folders[street + 1]
         net_file = net_file + arguments.value_net_name
         net_file = net_file + ".tar"
 
         return self.load_from_file(net_file)
-    
-    def load_river_model(self):
-        self.is_my_model = True
-        
-        # Load my model
-        model_name = "baseline_model_1653567423.pth.tar"
-        arguments.timer.split_start(f"Loading neural network '{model_name}'", log_level="DEBUG")
-        river_model_path = arguments.street_folders[4] + model_name
-        
-        self.model = BaselineNN()
-        self.model.load_state_dict(torch.load(os.path.abspath(arguments.model_path+river_model_path))["model"])
-        
-        if arguments.device == torch.device('cpu'):
-            self.model.cpu()
-        else:
-            self.model.cuda()
-            
-        self.model.eval()
-        
-        print("LOADED MY MODEL")
-        
-        return self
-        
 
     def load_from_file(self, file_name: str):
 
@@ -110,7 +78,6 @@ class ValueNn(object):
 
         # setting model to evaluation mode
         self.model.evaluate()
-        self.is_my_model = False
 
         arguments.timer.split_stop("Network loaded in", log_level="LOADING")
 
@@ -147,8 +114,3 @@ class ValueNn(object):
         arguments.logger.info(f"Saving model '{file_name}'")
         arguments.logger.debug(f"{repr(self)}")
         torch.save({'model_info': self.model_info, 'model': self.model, 'model_state': self.model_state}, file_name)
-
-
-
-
-
